ff1ffbcfcba593a8854d6eb60cd3a6e662c4fd34
ravi guntupalli <ravi@ravis-mbp.attlocal.net>
2016-10-24 20:30:11 -0500

    added logic for flattening array

---
+++ ArrayFlatten.js
@@ -0,0 +1,16 @@
+//This example demonstrates array flattening
+//This could be done easily using lodash.flatten and lodash.flattenDeep, how ever can be done also using plain java script
+function flattenArray(arrayToBeFlattened) {
+    var flattenedArray = [];
+    for(var i = 0; i < arrayToBeFlattened.length; i++) {
+        if(Array.isArray(arrayToBeFlattened[i])) {
+            flattenedArray = flattenedArray.concat(flatten(arrayToBeFlattened[i]));
+        } else {
+            flattenedArray.push(arrayToBeFlattened[i]);
+        }
+    }
+    return flattenedArray;
+}
+
+console.log(flatten([[1,2,[3]],4]));
+console.log(flatten([[[[1,2,3]]]]));
